06d3407b6974cf9036670e083beb9f52d0b4588f
arri-cc <arri@arri.io>
2021-07-14 18:32:13 -0400

    initial

---
+++ srv.py
@@ -0,0 +1,36 @@
+from google.cloud import dns
+import base64
+import json
+from types import SimpleNamespace
+
+f = open("event.json","r")
+text = f.read()
+data = json.loads(text, object_hook=lambda d: SimpleNamespace(**d))
+
+svc_name = data.protoPayload.request.service.metadata.name
+svc_url = data.protoPayload.request.service.status.url
+
+print(f'name: {svc_name} url: {svc_url}')
+
+def srv_pubsub(event, context):
+  print(f'eventid: {context.event_id} time: {context.timestamp} resource: {context.resource["name"]')
+
+  if 'data' in event:
+    data = base64.b64decode(event['data']).decode('utf-8')
+    audit_event = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+else:
+    print(f'no data available in event: {context.event_id}')
+
+svc_name = data.protoPayload.request.service.metadata.name
+svc_url = data.protoPayload.request.service.status.url
+svc_port = 443 #always the same for now
+
+project_name ='arri-primary'
+zone_name ='hrm.local'
+
+client = dns.Client(project=project_name)
+zone = client.zone(zone_name)
+
+print(f'zone: {zone_name} exists? {zone.exists()}')
+
+
