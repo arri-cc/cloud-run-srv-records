8677abd77d6873883418260b27d7fbebfd82c6b0
Arri Rucker <arri@arri.io>
2021-07-20 11:06:19 -0400

    dependencies

--- srv.py
+++ srv.py
@@ -3,27 +3,29 @@ import base64
 import json
 from types import SimpleNamespace
 
-print(f'name: {svc_name} url: {svc_url}')
 
-def srv_pubsub(event, context):
-  print(f'eventid: {context.event_id} time: {context.timestamp} resource: {context.resource["name"]')
+
+def audit_event(event, context):
+  print(f'eventid: {context.event_id} time: {context.timestamp} resource: {context.resource["name"]}')
 
   if 'data' in event:
     data = base64.b64decode(event['data']).decode('utf-8')
     audit_event = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-else:
+  else:
     print(f'no data available in event: {context.event_id}')
 
-svc_name = data.protoPayload.request.service.metadata.name
-svc_url = data.protoPayload.request.service.status.url
-svc_port = 443 #always the same for now
-
-project_name ='arri-primary'
-zone_name ='hrm.local'
-
-client = dns.Client(project=project_name)
-zone = client.zone(zone_name)
-
-print(f'zone: {zone_name} exists? {zone.exists()}')
+  svc_name = data.protoPayload.request.service.metadata.name
+  svc_url = data.protoPayload.request.service.status.url
+  svc_port = 443 #always the same for now
+  svc_project = 'arri-primary'
+  svc_zone = 'hrm.local'
+  print(f'name: {svc_name} url: {svc_url} port: {svc_port}')
 
 
+
+
+  print(f'zone: {zone_name} exists? {zone.exists()}')
+
+def create_srv_record(zone, name, url, port ):
+  client = dns.Client(project=project_name)
+  zone = client.zone(zone_name)
